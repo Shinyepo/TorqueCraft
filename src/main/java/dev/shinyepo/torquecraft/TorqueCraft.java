d9eec80d8dc02b56cc7a60915a569d96da16a9c3
Shinyepo <shinyepo42@gmail.com>
2024-04-29 11:56:22 +0200

    Main cleanup

--- src/main/java/dev/shinyepo/torquecraft/TorqueCraft.java
+++ src/main/java/dev/shinyepo/torquecraft/TorqueCraft.java
@@ -1,103 +1,36 @@
 package dev.shinyepo.torquecraft;
 
-import com.mojang.logging.LogUtils;
 import dev.shinyepo.torquecraft.menu.GrinderScreen;
 import dev.shinyepo.torquecraft.recipes.TorqueRecipes;
 import dev.shinyepo.torquecraft.registries.*;
-import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(TorqueCraft.MODID)
 public class TorqueCraft
 {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "torquecraft";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
-    // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
-//    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", TUNGSTEN_BLOCK);
 
-
-    // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
-
-
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public TorqueCraft(IEventBus modEventBus)
     {
-        // Register the commonSetup method for modloading
-//        modEventBus.addListener(this::commonSetup);
-
-        // Register the Deferred Register to the mod event bus so blocks get registered
         TorqueBlocks.BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        TorqueItems.ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
-        TorqueCreativeTabs.CREATIVE_MODE_TABS.register(modEventBus);
-
         TorqueBlockEntities.BLOCK_ENTITIES.register(modEventBus);
-
+        TorqueItems.ITEMS.register(modEventBus);
+        TorqueCreativeTabs.CREATIVE_MODE_TABS.register(modEventBus);
         TorqueMenus.MENU_TYPES.register(modEventBus);
-
         TorqueRecipes.Types.RECIPE_WRITING.register(modEventBus);
         TorqueRecipes.Serializers.RECIPE_SERIALIZERS.register(modEventBus);
 
         modEventBus.addListener(TorqueCapabilities::registerCapabilities);
-
-        // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
-        NeoForge.EVENT_BUS.register(this);
-
-
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-//        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-//    private void commonSetup(final FMLCommonSetupEvent event)
-//    {
-//        // Some common setup code
-//        LOGGER.info("HELLO FROM COMMON SETUP");
-//
-//        if (Config.logDirtBlock)
-//            LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
-//
-//        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-//
-//        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
-//    }
-
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-        }
-
         @SubscribeEvent
         public static void menuSetup(RegisterMenuScreensEvent e) {
             e.register(TorqueMenus.GRINDER_CONTAINER.get(), GrinderScreen::new);
