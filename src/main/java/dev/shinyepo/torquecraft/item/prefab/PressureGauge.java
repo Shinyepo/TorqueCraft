f6d2dea87aa15bc44aa1fcdaac717cabbcf5a3f5
Shinyepo <shinyepo42@gmail.com>
2024-05-05 13:54:57 +0200

    properly check fluid contents + missing lang input

--- src/main/java/dev/shinyepo/torquecraft/item/prefab/PressureGauge.java
+++ src/main/java/dev/shinyepo/torquecraft/item/prefab/PressureGauge.java
@@ -1,15 +1,9 @@
 package dev.shinyepo.torquecraft.item.prefab;
 
-import dev.shinyepo.torquecraft.block.entities.PumpEntity;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Fluids;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.fluids.FluidStack;
@@ -22,18 +16,16 @@ public class PressureGauge extends Item {
 
     @Override
     public InteractionResult useOn(UseOnContext context) {
-        if(!context.getLevel().isClientSide()) {
-            return InteractionResult.CONSUME;
+        IFluidHandler handler = context.getLevel().getCapability(Capabilities.FluidHandler.BLOCK,context.getClickedPos(),null);
+        if(handler != null) {
+            boolean isClient = context.getLevel().isClientSide();
+            if (isClient) {
+                FluidStack fluid = handler.getFluidInTank(0);
+                Component displayName = fluid.getFluid().isSame(Fluids.EMPTY) ? Component.literal("Empty") : fluid.getHoverName();
+                context.getPlayer().displayClientMessage(Component.translatable("torquecraft.chatmsg.fluid_content", displayName, fluid.getAmount()), true);
+            }
+            return InteractionResult.SUCCESS;
         }
-        BlockPos blockPos = context.getClickedPos();
-        BlockEntity blockEntity = context.getLevel().getBlockEntity(blockPos);
-        Direction face = context.getClickedFace();
-        IFluidHandler handler = context.getLevel().getCapability(Capabilities.FluidHandler.BLOCK, blockPos, context.getLevel().getBlockState(blockPos), blockEntity, face);
-        if (handler != null) {
-            FluidStack fluid = handler.getFluidInTank(handler.getTanks());
-            Component displayName = fluid.getFluid().isSame(Fluids.EMPTY) ? Component.literal("Empty") : fluid.getHoverName();
-            context.getPlayer().displayClientMessage(Component.translatable("torquecraft.chatmsg.fluid_content",displayName, fluid.getAmount()),true);
-        }
-        return InteractionResult.SUCCESS;
+        return InteractionResult.FAIL;
     }
 }
