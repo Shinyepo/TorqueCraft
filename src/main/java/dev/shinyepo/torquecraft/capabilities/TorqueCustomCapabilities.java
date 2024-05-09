1a81c1243981afce060897497524e8e485a445d4
Shinyepo <shinyepo42@gmail.com>
2024-05-09 21:28:10 +0200

    add custom capabilities

---
+++ src/main/java/dev/shinyepo/torquecraft/capabilities/TorqueCustomCapabilities.java
@@ -0,0 +1,13 @@
+package dev.shinyepo.torquecraft.capabilities;
+
+import dev.shinyepo.torquecraft.TorqueCraft;
+import dev.shinyepo.torquecraft.capabilities.types.IRotaryHandler;
+import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.capabilities.BlockCapability;
+import org.jetbrains.annotations.Nullable;
+
+public class TorqueCustomCapabilities {
+
+    public static final BlockCapability<IRotaryHandler, @Nullable Direction> ROTARY_HANDLER_BLOCK = BlockCapability.createSided(new ResourceLocation(TorqueCraft.MODID, "rotary_handler"), IRotaryHandler.class);
+}
