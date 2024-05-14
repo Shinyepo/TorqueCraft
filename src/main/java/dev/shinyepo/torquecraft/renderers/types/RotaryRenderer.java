7474a082c76ce6bd3228651109eafe0c33686b16
Shinyepo <shinyepo42@gmail.com>
2024-05-14 22:31:37 +0200

    rotation calc for source and transmitter

--- src/main/java/dev/shinyepo/torquecraft/renderers/types/RotaryRenderer.java
+++ src/main/java/dev/shinyepo/torquecraft/renderers/types/RotaryRenderer.java
@@ -5,6 +5,7 @@ import dev.shinyepo.torquecraft.TorqueCraft;
 import dev.shinyepo.torquecraft.capabilities.TorqueCustomCapabilities;
 import dev.shinyepo.torquecraft.capabilities.handlers.IRotaryHandler;
 import dev.shinyepo.torquecraft.factory.rotary.RotarySource;
+import dev.shinyepo.torquecraft.factory.rotary.RotaryTransmitter;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
@@ -33,8 +34,10 @@ public class RotaryRenderer {
         pose.pushPose();
         pose.translate(0.5F, 0.5F, 0.5F);
         double angle = 0;
-        if (blockEntity instanceof RotarySource ro) {
-            angle = (ro.getAngle() + (handler.getAngular() / 10) * partialTick) % 360;
+        if (blockEntity instanceof RotarySource rs) {
+            angle = -(rs.getAngle() + (handler.getAngular() / 10) * partialTick) % 360;
+        } else if (blockEntity instanceof RotaryTransmitter rt) {
+            angle = (rt.getAngle() + (handler.getAngular() / 10) * partialTick) % 360;
         }
         pose.mulPose(new Quaternionf().rotateAxis((float)Math.toRadians(angle % 360), new Vector3f(-direction.getStepX(), 0, -direction.getStepZ())));
         pose.mulPose(direction.getRotation());
