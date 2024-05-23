b4d54fe959b0102e09cf0730c4bb22220d3a6b3b
Shinyepo <shinyepo42@gmail.com>
2024-05-23 16:44:44 +0200

    heat sources tag

---
+++ src/main/java/dev/shinyepo/torquecraft/registries/tag/TorqueTags.java
@@ -0,0 +1,11 @@
+package dev.shinyepo.torquecraft.registries.tag;
+
+import dev.shinyepo.torquecraft.TorqueCraft;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+
+public class TorqueTags {
+    public static final TagKey<Block> HEAT_SOURCE = TagKey.create(Registries.BLOCK, new ResourceLocation(TorqueCraft.MODID, "heat_sources"));
+}
