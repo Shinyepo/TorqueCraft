6fc4e4295dde769756fb4c65096b7849e9525906
Shinyepo <shinyepo42@gmail.com>
2024-05-28 20:55:05 +0200

    Tank cap cache

--- src/main/java/dev/shinyepo/torquecraft/block/entities/FluidTankEntity.java
+++ src/main/java/dev/shinyepo/torquecraft/block/entities/FluidTankEntity.java
@@ -1,19 +1,34 @@
 package dev.shinyepo.torquecraft.block.entities;
 
-import dev.shinyepo.torquecraft.factory.MachineFactory;
-import dev.shinyepo.torquecraft.registries.block.TorqueBlockEntities;
 import dev.shinyepo.torquecraft.factory.TorqueFluidTank;
+import dev.shinyepo.torquecraft.networking.TorqueMessages;
+import dev.shinyepo.torquecraft.networking.packets.SyncFluidS2C;
+import dev.shinyepo.torquecraft.registries.block.TorqueBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.capabilities.BlockCapabilityCache;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.fluids.FluidStack;
 import net.neoforged.neoforge.fluids.capability.IFluidHandler;
+import org.jetbrains.annotations.Nullable;
 
-public class FluidTankEntity extends MachineFactory {
+public class FluidTankEntity extends BlockEntity {
     private final int fluidCapacity = 64000;
 
-    private final TorqueFluidTank fluidTank = createFluidTank(fluidCapacity);
+    private final TorqueFluidTank fluidTank = new TorqueFluidTank(fluidCapacity) {
+        @Override
+        protected void onContentsChanged() {
+            setChanged();
+            if(!level.isClientSide()) {
+                TorqueMessages.sendToAllPlayers(new SyncFluidS2C(worldPosition, this.fluid));
+            }
+        }
+    };
+
+    private BlockCapabilityCache<IFluidHandler, @Nullable Direction> capCache;
 
     public FluidTankEntity(BlockPos pPos, BlockState pBlockState) {
         super(TorqueBlockEntities.FLUID_TANK_ENTITY.get(), pPos, pBlockState);
@@ -27,7 +42,7 @@ public class FluidTankEntity extends MachineFactory {
         if (fluidTank.isEmpty()) {
             return;
         }
-        IFluidHandler fHandler = level.getCapability(Capabilities.FluidHandler.BLOCK, getBlockPos().relative(Direction.DOWN),null);
+        IFluidHandler fHandler = this.capCache.getCapability();
         if (fHandler != null) {
             if (!canAcceptFluid(fHandler)) return;
             int remainingSpace = fHandler.getTankCapacity(0) - fHandler.getFluidInTank(0).getAmount();
@@ -49,6 +64,17 @@ public class FluidTankEntity extends MachineFactory {
             return fluidTank;
         }
         return null;
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        this.capCache = BlockCapabilityCache.create(
+                Capabilities.FluidHandler.BLOCK,
+                (ServerLevel) this.getLevel(),
+                this.worldPosition.relative(Direction.DOWN),
+                null
+        );
     }
 
     public void setFluidStack(FluidStack fluidStack) {
